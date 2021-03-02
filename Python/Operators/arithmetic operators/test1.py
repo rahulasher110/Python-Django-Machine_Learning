545f655ccfcb4222e5e2b10e224b61c827670d99
rahulasher110 <rahulasher110@gmail.com>
2021-03-02 13:19:56 +0530

    arithmetic operators
    Signed-off-by: rahulasher110 <rahulasher110@gmail.com>

---
+++ Python/Operators/arithmetic operators/test1.py
@@ -0,0 +1,12 @@
+# arithmetic
+
+x = 10
+y = 3
+
+print('x + y :- ',x+y)
+print('x - y :- ',x-y)
+print('x * y :- ',x*y)
+print('x / y :- ',x/y)
+print('x // y :- ',x//y) #floor division
+
+print('x ** y :- ',x**y)
